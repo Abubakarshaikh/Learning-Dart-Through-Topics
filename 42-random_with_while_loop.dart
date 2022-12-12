e1c5f5de887ea7d9158b46d34b6c0b934a5bb4ff
abubakarshaikh <atifa2019@gmail.com>
2022-12-12 13:31:59 +0500

    added new topics

---
+++ 42-random_with_while_loop.dart
@@ -0,0 +1,37 @@
+// Example #1 - Random Integer Number Zero(0) to Five(5):
+// import 'dart:math';
+
+// void main() {
+//   final Random random = Random();
+
+//   final int randomIntegerZeroToFive = random.nextInt(5) + 1;
+//   print(randomIntegerZeroToFive);
+// }
+
+// Example #2 - Random Double Number Zero(0.0) to One(1.0):
+// import 'dart:math';
+// void main() {
+//   final Random random = Random();
+//   final double randomIntegerZeroToFive = random.nextDouble();
+//   print(randomIntegerZeroToFive);
+// }
+
+// Example #3 - Random Boolean, true/false:
+// import 'dart:math';
+
+// void main() {
+//   final Random random = Random();
+//   final bool randomIntegerZeroToFive =  random.nextBool();
+//   print(randomIntegerZeroToFive);
+// }
+
+// Example #4 - Random Integer With While Loop:
+// import 'dart:math';
+
+// void main() {
+//   final Random random = Random();
+//   while (random.nextInt(10) + 1 != 6) { // 6 != 6
+//     print('Not a six!');
+//   }
+//   print('Finally, you got a six!');
+// }
---
+++ 43-for_loops.dart
@@ -0,0 +1,57 @@
+// // Example #1: Difference Between While & For loop
+// void main() {
+// var i = 0;
+//   while (i < 3) {
+//     print("Count Value: $i ");
+//     i++;
+//   }
+
+//   for (var i = 0; i < 3; i++) {
+//     print("Count Value: $i ");
+//   }
+// }
+
+// // Example #2: For Loop Reversed
+// void main() {
+//   for (var i = 3; i > 0; i--) {
+//     print("Count Value: $i ");
+//   }
+// }
+
+// // Example #3: Icrement Before Print & After Print
+// void main() {
+//   for (var i = 0; i < 3; i++) {
+//     print("Count Value: $i ");
+//   }
+// }
+
+// Example #4: For Loop Parameter Practice
+// void main() {
+  // var i = 0;
+  // for (; i < 3; i++) {
+  //   print("Count Value: $i ");
+  // }
+
+  // for (; i < 3; i++) {
+  //   print("Count Value: $i ");
+  // }
+
+  // for (; i < 3; i++) {
+  //   print("Count Value: $i ");
+  //   i = 1;
+  // }
+
+  // for (;; i++) {
+  //   i < 3;
+  //   print("Count Value: $i ");
+  // }
+// }
+
+// // Example #5: Star Print Using For Loop
+void main() {
+  String star = "";
+  for (var i = 0; i < 3; i++) {
+    star += "*";
+    print(star);
+  }
+}
