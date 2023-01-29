956ff4e2a272a78d4df53f81ce3ad71ae558e44f
abubakarshaikh <atifa2019@gmail.com>
2023-01-29 12:57:35 +0500

    added writing good functions

---
+++ 49-writing-good-functions.dart
@@ -0,0 +1,11 @@
+// Avoiding side effects
+// pure functions.
+// unknown side effect
+// Doing only one thing
+// Choosing good names
+// unknown parameter type
+void main() {}
+
+double averagePercentage() {
+  return 12.5;
+}
