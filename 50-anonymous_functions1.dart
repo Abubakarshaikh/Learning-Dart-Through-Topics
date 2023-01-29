200289653f4f3ba75abd37917d41ffb8a22e9dcb
abubakarshaikh <atifa2019@gmail.com>
2023-01-29 12:58:06 +0500

    added annonymouse fuction

---
+++ 50-anonymous_functions1.dart
@@ -0,0 +1,12 @@
+// Assigning functions to variables
+
+int someIntg = 34;
+double someDouble = 8.9;
+
+Function someValue = (int value) {
+  return value;
+};
+
+void main() {
+  print(someValue(12));
+}
