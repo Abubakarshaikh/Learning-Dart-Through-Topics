354fbcbadd1a261a94acb02f42cbfa090bde7121
Abubakar Shaikh <atifa2019@gmail.com>
2023-03-02 12:58:21 +0500

    added 71-static_methods

---
+++ 71-static_methods.dart
@@ -0,0 +1,13 @@
+// void main() {
+//   User user1 = User();
+//   User user2 = User.Anonymouse();
+//   User user3 = User.fromJson();
+
+//   User user4 = User.fromMap();
+// }
+
+// class User {
+
+
+//   const static String isEven()  {}
+// }
