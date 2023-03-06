3d2e2d90f5e830ff048a8d8dad25621e2656beb8
Abubakar Shaikh <atifa2019@gmail.com>
2023-03-06 23:05:53 +0500

    added 72 topics

---
+++ 72-nullability.dart
@@ -0,0 +1,16 @@
+// [horse, camel, pig, cow, sheep, goat] //pig
+// [196, 144, 169, 182, 121] // 182
+// [3, null, 1, 7, 4, 5] //
+
+void main() {
+  // < 2.12
+  // String? userInput = null;
+  // int? phoneNumber = null;
+
+  // 50%
+
+// Sound null safety
+  int? postalCode = null; // Time bom
+
+  
+}
